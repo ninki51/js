5a35af1efeaeb6a671e98ffa1526202de4fb155a
剑客 <2556792125@qq.com>
2018-07-02 14:49:01 +0800

    Update game.js

--- game.js
+++ game.js
@@ -12,41 +12,44 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        //游戏状态：0=准备发射钩子,1=发射,2=回收钩子
-        GameStatus: 0,
-        hook: {
-            default: null,
-            type: cc.Node
-        },
-
+        HookStatus: 0
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
-        this.GameStatus = 0;
-        this.star = cc.find('Canvas/star');
-        this.hook = cc.find('Canvas/miner/hook');
+        this.point = cc.find('Canvas/bg/hook/point');
+        this.hook = cc.find('Canvas/bg/hook');
         this.hook.rotation = 0.0;
-        this.hook.height = 10;
-        cc.log('------------------------------------------------');
+        this.wall = cc.find('Canvas/wall');
+        //记录钩子长度
+        this.HookHeight = 100;
+        this.hook.height = this.HookHeight;
+        
+
         this.node.on(cc.Node.EventType.TOUCH_END, function (event) {
             console.log('=========clickBox touchend==============');
-            // console.log(event.touch.getLocation().x);
-            // console.log(event.touch.getLocation().y);
-            //this.emitClick();
-            //this.node.dispatchEvent(new cc.Event.EventCustom('sayHello',true));
-            if (this.GameStatus === 0) {
+            if (this.HookStatus === 0) {
                 let temp = event.getLocation();
                 let that = this;
                 let xy = that.node.convertToNodeSpaceAR(temp);
-                this.node.emit('touchXY', {
-                    // x: event.touch.getLocation().x, y: event.touch.getLocation().y
+                that.node.emit('touchXY', {
                     xy: xy
                 });
-                this.GameStatus = 1;
+                that.HookStatus = 1;
             }
         }, this);
+
+        let manager = cc.director.getCollisionManager();
+        manager.enabled = true;
+        //manager.enabledDebugDraw = true;
+        //manager.enabledDrawBoundingBox = true;
+
+        this.node.on('Collision', function (event) {
+            cc.log('-----------------接收到碰撞消息------------------');
+            this.HookStatus = 2;
+            //this.PullBackHook();
+        }.bind(this));
 
     },
 
@@ -58,29 +61,21 @@ cc.Class({
             let touchY = parseFloat(event.detail.xy.y);
             cc.log(touchX, touchY);
             this.calcAngle(touchX, touchY);
-            //this.hook.height = 200;
+            
             //event.stopPropagation();
 
         }.bind(this));
     },
 
-
     calcAngle(touchX, touchY) {
         //左侧+1，右侧-1
-        let hookCenterX = this.hook.position.x + this.hook.parent.position.x - 5;
-        let hookCenterY = this.hook.position.y + this.hook.parent.position.y - 10;
+        let hookCenterX = this.hook.position.x + this.hook.parent.position.x ;
+        let hookCenterY = this.hook.position.y + this.hook.parent.position.y ;
         //hookCenterX=13.91 hookCenterY=160.51999999999998
-        this.star.x = hookCenterX;
-        this.star.y = hookCenterY;
         let angle = 0.0;
         cc.log('hookCenterX=' + hookCenterX, 'hookCenterY=' + hookCenterY);
         cc.log('touchX=' + touchX + ',touchY=' + touchY);
         let dx = Math.abs(touchX - hookCenterX);
-        // if (touchX > 0.0){
-        //     dx = touchX - hookCenterX;
-        // }else if(touchX === 0.0){
-        //     angle = 0.0;
-        // }
         let dy = Math.abs(touchY - hookCenterY);
         cc.log('dx=' + dx + ',dy=' + dy);
 
@@ -98,44 +93,83 @@ cc.Class({
             cc.log('angle=' + angle);
         }
         this.hook.rotation = angle;
-
-    },
-    
-    emitHook(){
-        let self = this;
-        if (self.GameStatus === 1){
-            self.hook.height += 1;
-            if (self.hook.height > 350){
-                self.GameStatus = 2;
-            }
-        }else if (self.GameStatus === 2){
-            self.hook.height -= 1;
-            if (self.hook.height < 10){
-                self.GameStatus = 0;
-            }
-        }
         
+        this.moveHook(angle);
     },
+
+    //以一个角度移动钩子
+    moveHook(angle) {
+        let step = 1.0;
+        let speed = 1;
+        //let angle = this.hook.rotation;
+        let radian = angle * 3.14159 / 180.0;
+        cc.log('angle=' + angle);
+        // let dx = Math.sin(radian) * step;
+        // let dy = Math.cos(radian) * step;
+        // this.hook.height += 300;
+        // cc.log('dx=' + dx + ',dy=' + dy);
+        // let moveAction = cc.moveTo(speed, cc.p(dx, dy));
+        // this.hook.runAction(moveAction);
+        // this.hook.x -= Math.sin(radian) * speed;
+        // this.hook.y -= Math.cos(radian) * speed;
+        this.hook.height += speed;
+    },
+
+    PullBackHook() {
+        //播放拉回钩子动画
+        //this.MinerAnimation.play('PullRope');
+        // let moveAction = cc.moveTo(1, cc.p(this.startX, this.startY));
+        // this.hook.runAction(moveAction);
+        //this.hook.height = this.HookHeight;
+        //this.HookStatus = 2;
+    },
+
+    Handle(items) {
+        this.AddScore(items);
+        this.RemoveItem(items);
+    },
+    /**
+     * @description 删除物品
+     */
+    RemoveItem(items) {
+        items.forEach(item => {
+            item.destroy();
+        });
+    },
+    /**
+     * @description 添加得分
+     */
+    AddScore(items) {
+        if (!items[0]) return;
+        // let scoreCon = ItemAttr[items[0].name] || {};
+        // this.Score.string = parseInt(this.Score.string) + (scoreCon.score || 0);
+        // //播放得分音效
+        // cc.audioEngine.play(this.AddScroeAudio);
+    },
+
 
     update(dt) {
-        // if(this.GameStatus == 0){
-        //     this.node.on(cc.Node.EventType.TOUCH_END, function (event) {
-        //         console.log('========touchend==============');
-        //         console.log(event.touch.getLocation().x);
-        //         console.log(event.touch.getLocation().y);
-        //         this.calcAngle(event.touch.getLocation().x,event.touch.getLocation().y);
-        //         this.GameStatus = 1;
-        //     }, this);
-        // }else if(this.GameStatus == 1 && this.hook.height < 300){
-        //     this.hook.height += 1;
-        // }else if(this.GameStatus == 1 && this.hook.height > 300){
-        //     this.GameStatus = 2;
-        // }else if(this.GameStatus == 2 && this.hook.height > 30){
-        //     this.hook.height -= 1;
-        // }else if (this.hook.height <= 30){
-        //     this.GameStatus = 0;
-        // }
-        //this.hook.height = 350;
-        this.emitHook();
+        if (this.HookStatus === 0){
+            return
+        }else if(this.HookStatus === 1){
+            //发射钩子
+            this.hook.height += 1;
+        }else if(this.HookStatus === 2){
+            //收回钩子
+            if(this.hook.height > this.HookHeight){
+                this.hook.height -= 1;
+            }else{
+                //回收到位
+                this.HookStatus = 0;
+                //检测是否拉回物品
+                cc.log('检测是否拉回物品=====')
+                cc.log(this.hook.children[1].childrenCount)
+                if (this.hook.children[1].childrenCount) {
+                    this.Handle(this.hook.children[1].children);
+                };
+            }
+            
+        }
+
     },
 });
