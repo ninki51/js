585cce462b82608ce3e5ce3afa8fca116922c665
剑客 <2556792125@qq.com>
2018-07-12 14:22:35 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,12 +1,3 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
 
 cc.Class({
     extends: cc.Component,
@@ -19,47 +10,52 @@ cc.Class({
 
     onLoad() {
         //监听碰撞
-        this.onCollisionEnter = this.onCollisionEnterA;
+        //this.onCollisionEnter = this.onCollisionEnterA
         //this.Canvas = cc.find('Canvas');
-        this.game = cc.find('Canvas').getComponent('game');
+        this.main = cc.find('Canvas').getComponent('main')
+        //挂载金块的节点
+        this.temp = cc.find('Canvas/bg/hero/car/rope/temp')
+        this.rope = cc.find('Canvas/bg/hero/car/rope')
+        this.left = cc.find('Canvas/bg/hero/car/rope/left')
+        // this.glod_1 = cc.find('Canvas/bg/move_layer/glod_1').getComponent(cc.Sprite)
+        // this.glod_2 = cc.find('Canvas/bg/move_layer/glod_2').getComponent(cc.Sprite)
+        // this.glod_3 = cc.find('Canvas/bg/move_layer/glod_3').getComponent(cc.Sprite)
+        //cc.log(this.glod_1)
+    },
+
+    start() {
+
+    },
+    onCollisionEnter: function (other, self) {
+        cc.log('碰撞检测22222=========')
+        if (this.main.HookStatus > 1)
+            return
+        this.isWall = this.Wall(other)
+        this.isGlod = this.Glod(other)
+        this.isStone = this.Stone(other)
+
+        if (this.isWall || this.isStone) {
+            //碰到墙和石头弹回
+            cc.find('Canvas').emit('Collision', { catch: 0 })
+        } else if (this.isGlod) {
+            cc.log('碰撞金矿===============================')
+            let glod = other.getComponent(other.node.name)
+            glod.status = 0
+            //根据物品设置拉回钩子速度
+            this.main.SetSpeed(glod.pollSpeed)
+            // let xy = this.node.convertToNodeSpaceAR(this.temp)
+            // cc.log(xy.x,xy.y)
+            other.node.x = -this.rope.width * 0.5
+            other.node.y = this.temp.y + this.rope.y + this.rope.parent.y + this.rope.parent.parent.y + this.rope.parent.parent.parent.y + other.node.height + 10
+
+            other.node.parent = this.temp
+            other.node.anchorY = 1.0
+            cc.find('Canvas').emit('Collision', { catch: 1 })
+        }
+
 
     },
 
-    onCollisionEnterA(other, self) {
-        console.log('碰撞检测=========');
-        //if (self.game.GameStatus == 2) return;
-        this.other = other;
-        this.isWall = this.Wall(other);
-        this.isGlod = this.Glod(other);
-        this.isStone = this.Stone(other);
-        //处理钩子撞墙
-        if (this.isWall || this.isGlod || this.isStone) {
-            if (this.isWall) {
-                cc.log('碰撞墙===============================');
-            } else if (this.isGlod) {
-                cc.log('碰撞金矿===============================');
-                //将物品放置钩子上
-                cc.log('抓住===================');
-                cc.log(this.other.node.x, this.other.node.y);
-                other.node.parent = this.game.point;
-                this.other.node.y = this.game.hook.y -130
-                this.other.node.x = this.game.hook.x -30
-                // this.other.node.y = -(this.game.hook.height + 2);
-                // this.other.node.x = -(this.game.hook.width / 2);
-
-                other.node.anchorY = 1.0;
-                cc.log(this.other.node.x, this.other.node.y);
-            } else if (this.isStone) {
-                cc.log('碰撞石头===============================');
-
-            }
-
-            this.node.parent.parent.parent.emit('Collision', {});
-            return;
-        };
-
-
-    },
     Wall(other) {
         return other.node.group == 'wall';
     },
@@ -69,4 +65,6 @@ cc.Class({
     Stone(other) {
         return other.node.group == 'stone';
     },
+
+    // update (dt) {},
 });
