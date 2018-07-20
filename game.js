d783f3a6e4b3186a6fa31616f895597708984bef
剑客 <2556792125@qq.com>
2018-07-20 16:52:55 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,175 +1,185 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+var hook = require('hook')
 
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        HookStatus: 0
+        total_time: {
+            default: 120,
+            displayName: '游戏总时间'
+        },
+        hookPrefab: {
+            default: [],
+            type: cc.Prefab
+        },
+
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
-        this.point = cc.find('Canvas/bg/hook/point');
-        this.hook = cc.find('Canvas/bg/hook');
-        this.hook.rotation = 0.0;
-        this.wall = cc.find('Canvas/wall');
-        //记录钩子长度
-        this.HookHeight = 100;
-        this.hook.height = this.HookHeight;
+        //爪子数组
+        //let hookArray = cc.sys.localStorage.getItem('hookArray')
+        //背包等级
+        let bagLevel = cc.sys.localStorage.getItem('bagLevel')
+        //矿车等级
+        let minerLevel = cc.sys.localStorage.getItem('minerLevel')
+
+
+        this.hero = cc.find('Canvas/bg/hero')
+        this.hero.x = 800
+        this.bg = cc.find('Canvas/bg')
+        this.move_layer = cc.find('Canvas/bg/move_layer')
+        //放钩子得地方
+        this.hookPos = cc.find('Canvas/bg/hero/car/hook_pos')
+        this.hookData = [
+            { id: 0, speed: 100, rate: 0.3, angle: 0, status: 0 },
+            { id: 1, speed: 200, rate: 0.2, angle: 0, status: 0 },
+            { id: 2, speed: 300, rate: 0.1, angle: 0, status: 0 },
+        ]
+        this.hookArray = []
+
+        this.current_hook_id = 0
+        this.createHook()
+
         
-
-        this.node.on(cc.Node.EventType.TOUCH_END, function (event) {
-            console.log('=========clickBox touchend==============');
-            if (this.HookStatus === 0) {
-                let temp = event.getLocation();
-                let that = this;
-                let xy = that.node.convertToNodeSpaceAR(temp);
-                that.node.emit('touchXY', {
-                    xy: xy
-                });
-                that.HookStatus = 1;
+        //倒计时
+        this.timeLabel = cc.find('Canvas/bg/label_bar/timer_label').getComponent(cc.Label)
+        this.totalTime = 120
+        this.timeLabel.string = parseInt(this.totalTime)
+        this.schedule(function () {
+            this.totalTime--
+            this.timeLabel.string = parseInt(this.totalTime)
+            if (this.totalTime === 0) {
+                //cc.game.resume()
+                cc.game.pause()
             }
-        }, this);
+        }, 1);
+        //成绩
+        this.scoreLabel = cc.find('Canvas/bg/label_bar/score_label').getComponent(cc.Label)
+        this.score = 0
+        this.scoreLabel.string = parseInt(this.score)
+        this.node.on('score', function (event) {
+            let score = parseInt(event.detail.score)
+            this.setScore(score)
+        }.bind(this))
 
-        let manager = cc.director.getCollisionManager();
-        manager.enabled = true;
-        //manager.enabledDebugDraw = true;
-        //manager.enabledDrawBoundingBox = true;
-
-        this.node.on('Collision', function (event) {
-            cc.log('-----------------接收到碰撞消息------------------');
-            this.HookStatus = 2;
-            //this.PullBackHook();
-        }.bind(this));
+        //开启碰撞
+        let manager = cc.director.getCollisionManager()
+        manager.enabled = true
+        // manager.enabledDebugDraw = true
+        // manager.enabledDrawBoundingBox = true
 
     },
 
-    start() {
 
+    start() {
+        let action = cc.moveTo(1, cc.p(0, 277))
+        this.hero.runAction(action)
         this.node.on('touchXY', function (event) {
-            cc.log('-----------------接收到 touch坐标------------------');
-            let touchX = parseFloat(event.detail.xy.x);
-            let touchY = parseFloat(event.detail.xy.y);
-            cc.log(touchX, touchY);
-            this.calcAngle(touchX, touchY);
-            
-            //event.stopPropagation();
+            let hNode = this.hookArray[this.current_hook_id]
+            let theHook = hNode.getComponent(hook)
+            let status = theHook.status
+            cc.log('-----------------接收到 touch坐标------------------')
+            if (status === 0) {
+                let touchX = parseFloat(event.detail.xy.x)
+                let touchY = parseFloat(event.detail.xy.y)
+                //cc.log(touchX, touchY);
+                theHook.angle = this.calcAngle(touchX, touchY)
+                theHook.status = 1
+            }
+            //指向下一个钩子
+            this.nextHookId()
+            event.stopPropagation()
 
         }.bind(this));
+    },
+
+    //创建hook
+    createHook() {
+        for (let item of this.hookData) {
+            // let speed = item.speed
+            // let rate = item.rate
+            let id = item.id
+            if (id < 0 || id > 3) {
+                continue
+            }
+            let hNode = cc.instantiate(this.hookPrefab[id])
+            this.hookArray.push(hNode)
+        }
+        let angle = -45
+        for (let i = 0; i < this.hookArray.length; i++) {
+            let theHook = this.hookArray[i]
+            theHook.parent = this.hookPos
+            theHook.setPosition(this.hookPos.getPosition())
+            theHook.anchorX = 0.5
+            theHook.anchorY = 1.0
+            theHook.rotation = angle
+            let hookNode = theHook.getComponent(hook)
+            hookNode.speed = this.hookData[i].speed
+            hookNode.rate = this.hookData[i].rate
+            hookNode.angle = angle
+            angle += 45
+        }
+
+    },
+
+    setScore(s){
+        this.score += s
+        this.scoreLabel.string =parseInt(this.score)
+    },
+
+    update(dt) {
+
+
+
+    },
+
+    backHome() {
+        //返回首页
+        cc.director.loadScene("home");
+    },
+    //删除金块
+    delGlod() {
+        //挂金块的节点
+        let nums = cc.find('Canvas/bg/hero/car/hook_pos').childrenCount
+        if (nums) {
+            let glod = cc.find('Canvas/bg/hero/car/hook_pos').children[0]
+            let glod2 = glod.getComponent(glod.name)
+            glod.parent = this.move_layer
+            glod.setPosition(glod2.randomPos())
+            glod2.status = 1
+            //累加成绩
+            this.score += glod2.score
+            this.scoreLabel.string = parseInt(this.score)
+        }
+
     },
 
     calcAngle(touchX, touchY) {
         //左侧+1，右侧-1
-        let hookCenterX = this.hook.position.x + this.hook.parent.position.x ;
-        let hookCenterY = this.hook.position.y + this.hook.parent.position.y ;
+        touchX += this.move_layer.x + this.bg.x
+        touchY += this.move_layer.y + this.bg.y
+        let hookCenterX = this.hookPos.x + this.hookPos.parent.x + this.hookPos.parent.parent.x
+        let hookCenterY = this.hookPos.y + this.hookPos.parent.y + this.hookPos.parent.parent.y
         //hookCenterX=13.91 hookCenterY=160.51999999999998
-        let angle = 0.0;
-        cc.log('hookCenterX=' + hookCenterX, 'hookCenterY=' + hookCenterY);
-        cc.log('touchX=' + touchX + ',touchY=' + touchY);
-        let dx = Math.abs(touchX - hookCenterX);
-        let dy = Math.abs(touchY - hookCenterY);
-        cc.log('dx=' + dx + ',dy=' + dy);
+        let angle = 0.0
+        let dx = touchX - hookCenterX
+        let dy = touchY - hookCenterY
 
-        let symbol = -1;
-        if (touchX < hookCenterX) {
-            symbol = 1;
-        }
-        if (dx !== 0.0) {
-            angle = Math.atan(dy / dx) / 3.14159 * 180.0 * symbol;
-            if (angle < 90.0 && angle > 0.0) {
-                angle = 90 - angle;
-            } else if (angle < 0.0) {
-                angle = -(90 + angle);
-            }
-            cc.log('angle=' + angle);
-        }
-        this.hook.rotation = angle;
-        
-        this.moveHook(angle);
-    },
+        angle = Math.atan(dx / dy) / 3.14159 * 180.0
+        //cc.log('calc , angle=' + angle)
+        return angle
 
-    //以一个角度移动钩子
-    moveHook(angle) {
-        let step = 1.0;
-        let speed = 1;
-        //let angle = this.hook.rotation;
-        let radian = angle * 3.14159 / 180.0;
-        cc.log('angle=' + angle);
-        // let dx = Math.sin(radian) * step;
-        // let dy = Math.cos(radian) * step;
-        // this.hook.height += 300;
-        // cc.log('dx=' + dx + ',dy=' + dy);
-        // let moveAction = cc.moveTo(speed, cc.p(dx, dy));
-        // this.hook.runAction(moveAction);
-        // this.hook.x -= Math.sin(radian) * speed;
-        // this.hook.y -= Math.cos(radian) * speed;
-        this.hook.height += speed;
     },
-
-    PullBackHook() {
-        //播放拉回钩子动画
-        //this.MinerAnimation.play('PullRope');
-        // let moveAction = cc.moveTo(1, cc.p(this.startX, this.startY));
-        // this.hook.runAction(moveAction);
-        //this.hook.height = this.HookHeight;
-        //this.HookStatus = 2;
-    },
-
-    Handle(items) {
-        this.AddScore(items);
-        this.RemoveItem(items);
-    },
-    /**
-     * @description 删除物品
-     */
-    RemoveItem(items) {
-        items.forEach(item => {
-            item.destroy();
-        });
-    },
-    /**
-     * @description 添加得分
-     */
-    AddScore(items) {
-        if (!items[0]) return;
-        // let scoreCon = ItemAttr[items[0].name] || {};
-        // this.Score.string = parseInt(this.Score.string) + (scoreCon.score || 0);
-        // //播放得分音效
-        // cc.audioEngine.play(this.AddScroeAudio);
-    },
-
-
-    update(dt) {
-        if (this.HookStatus === 0){
-            return
-        }else if(this.HookStatus === 1){
-            //发射钩子
-            this.hook.height += 1;
-        }else if(this.HookStatus === 2){
-            //收回钩子
-            if(this.hook.height > this.HookHeight){
-                this.hook.height -= 1;
-            }else{
-                //回收到位
-                this.HookStatus = 0;
-                //检测是否拉回物品
-                cc.log('检测是否拉回物品=====')
-                cc.log(this.hook.children[1].childrenCount)
-                if (this.hook.children[1].childrenCount) {
-                    this.Handle(this.hook.children[1].children);
-                };
-            }
-            
+    nextHookId() {
+        this.current_hook_id++
+        let nums = this.hookArray.length
+        if (this.current_hook_id > nums - 1) {
+            this.current_hook_id = 0
         }
 
-    },
+        cc.log('下一个ID====' + this.current_hook_id)
+    }
+
 });
